7b7d629b1aa551a876c33de8068b4732dfba17cf
PRANAW2004 <98792065+PRANAW2004@users.noreply.github.com>
2023-03-25 19:16:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -105,10 +105,10 @@ function back(name1){
 var x = window.matchMedia("(max-width:1024px) and (min-width: 769px)")
 
 if(x.matches){
-    document.querySelector(".fa-pause-circle").classList.add("fa-5x");
-    document.querySelector(".play").classList.add("fa-5x");
+    document.querySelector(".btn1").classList.add("fa-5x");
+    document.querySelector(".btn2").classList.add("fa-5x");
 }
 else{
-    document.querySelector(".fa-pause-circle").classList.remove("fa-5x");
-    document.querySelector(".play").classList.remove("fa-5x");
+    document.querySelector(".btn1").classList.remove("fa-5x");
+    document.querySelector(".btn2").classList.remove("fa-5x");
 }
